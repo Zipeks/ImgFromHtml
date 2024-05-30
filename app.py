851f95cc0196430934d75922d52a2a5e02a0ade5
Mateusz Zieliński <matzielinski7@gmail.com>
2024-05-30 11:40:17 +0200

    Working GUI

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 anime = "onepiece.txt"
 
@@ -16,29 +16,46 @@ class GUI:
         self.window = tk.Tk()
 
         self.window.title("Down Img from HTML")
-        self.window.geometry("750x750")
+        self.window.geometry("750x250")
 
         self.download_frame = tk.Frame(self.window)
-        self.download_frame.pack(side="top", pady=10, padx=5)
+        self.download_frame.pack(side="top", pady=20, padx=5)
 
-        self.html_text = tk.Text(self.download_frame, height=10, width=50)
+        self.html_text = tk.Text(self.download_frame, height=10, width=40)
         self.html_text.grid(row=0, padx=5)
 
         self.buttons_frame = tk.Frame(self.download_frame)
         self.buttons_frame.grid(row=0, column=1)
 
+        self.save_location_frame = tk.Frame(self.buttons_frame)
+        self.save_location_frame.grid(row=0)
+
         self.save_location = os.getcwd()
 
+        self.down_label = tk.Label(self.save_location_frame, text="Download to:")
+        self.down_label.pack(side="top")
+
         self.save_location_Btn = tk.Button(
-            self.buttons_frame,
-            text=f"Download directory: {self.save_location}",
+            self.save_location_frame,
+            text=f"{self.save_location}",
             command=self.change_save_location,
         )
 
-        self.save_location_Btn.grid(row=0)
+        self.save_location_Btn.pack(side="top")
 
-        self.download_button = tk.Button(self.buttons_frame, text="Download")
-        self.download_button.grid(row=1, column=0, pady=5)
+        self.naming_frame = tk.Frame(self.buttons_frame)
+        self.naming_frame.grid(row=1)
+
+        self.names_label = tk.Label(self.naming_frame, text="Naming convention:")
+        self.names_label.grid(row=0, column=0)
+
+        self.names_input = tk.Entry(self.naming_frame)
+        self.names_input.grid(row=0, column=1)
+
+        self.download_button = tk.Button(
+            self.buttons_frame, text="Download", command=self.download_images
+        )
+        self.download_button.grid(row=2, column=0, pady=5)
         self.window.mainloop()
 
     def change_save_location(self):
@@ -47,7 +64,7 @@ class GUI:
             initialdir=self.save_location,
             title="Select where to save files",
         )
-        self.save_location_Btn.config(text=f"Download to: {self.save_location}")
+        self.save_location_Btn.config(text=f"{self.save_location}")
 
     def extract_image_urls(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
@@ -55,49 +72,28 @@ class GUI:
         img_urls = [img["src"] for img in img_tags]
         return img_urls
 
-    # Function to download images given their URLs
-    def download_images(self, img_urls, output_folder):
+    def download_images(self):
+        html_text = self.html_text.get("1.0", tk.END)
+        img_urls = self.extract_image_urls(html_text)
 
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+        output_folder = self.save_location
+        names = self.names_input.get()
 
         for i, img_url in enumerate(img_urls):
             response = requests.get(img_url, verify=False)
             if response.status_code == 200:
                 with open(
-                    os.path.join(output_folder, f"{filePrefix}_{chapter}_{i+1}.jpg"),
+                    os.path.join(output_folder, f"{names}_{i+1}.jpg"),
                     "wb",
                 ) as img_file:
                     img_file.write(response.content)
-                    print(f"Downloaded {filePrefix}_{chapter}_{i+1}.jpg")
+                    print(f"Downloaded {names}_{i+1}.jpg")
             else:
-                print(f"Failed to download {img_url}")
+                messagebox.showerror(
+                    "Something went wrong", f"Failed to download {img_url}"
+                )
 
-        # Read the HTML text from the file
-        file_path = "ToExtractUrl.txt"  # Replace 'your_input_file.txt' with the path to your input file
-        with open(file_path, "r", encoding="utf8") as file:
-            html_text = re.sub(r"<noscript>.*?</noscript>", "", file.read())
-
-        # Extract image URLs
-        image_urls = self.extract_image_urls(html_text)
-
-        # Download images to a folder
-        with open(anime, "r") as f:
-            output_folder = (
-                f.readline().rstrip()
-            )  # Replace 'downloaded_images' with your desired output folder name
-            filePrefix = f.readline().rstrip()
-            chapter = f.readline().rstrip()
-
-        self.download_images(image_urls, output_folder)
-
-        lines = []
-
-        with open(anime, "r") as f:
-            lines = f.readlines()[:-1]
-            lines.append(f"{int(chapter) + 1}")
-        with open(anime, "w") as f:
-            f.writelines(lines)
+        messagebox.showinfo("Success", f"Downloaded {i+1} files")
 
 
 window = GUI()
